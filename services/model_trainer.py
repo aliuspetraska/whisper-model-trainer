fc2082de71c37a49b84f504609d18b54ebaaadcd
Alius Petraška <alius.petraska@ibm.com>
2024-02-14 16:49:30 +0200

    updateds

--- services/model_trainer.py
+++ services/model_trainer.py
@@ -4,7 +4,7 @@ import os
 
 import evaluate
 import torch
-from datasets import DatasetDict, load_dataset, Audio
+from datasets import DatasetDict, load_dataset, Audio, IterableDatasetDict
 from huggingface_hub import login
 from transformers import WhisperFeatureExtractor, WhisperTokenizer, WhisperProcessor, WhisperForConditionalGeneration, \
     Seq2SeqTrainingArguments, Seq2SeqTrainer
@@ -77,12 +77,12 @@ class ModelTrainerService:
                 print("1")
 
                 self.common_voice["train"] = load_dataset("mozilla-foundation/common_voice_16_1", "lt", split="train",
-                                                          token=True, trust_remote_code=True, streaming=True)
+                                                          token=True, trust_remote_code=True)
 
                 print("2")
 
                 self.common_voice["test"] = load_dataset("mozilla-foundation/common_voice_16_1", "lt", split="test",
-                                                         token=True, trust_remote_code=True, streaming=True)
+                                                         token=True, trust_remote_code=True)
 
                 print("3")
 
